31f9fbfc113acc975d50b323241fa8e9e463e9c7
swarn <swarnchahal@Swarns-MacBook-Pro.local>
2021-04-14 19:56:08 -0400

    add subtrees based on the user response

--- src/commands/welcome.ts
+++ src/commands/welcome.ts
@@ -14,6 +14,7 @@ export default class Welcome extends Command {
     let directory
     let isAppsmith
     let isRabbitMQ
+    let isContent
     if (!directory) {
       const responses: any = await inquirer.prompt([{
         name: 'name',
@@ -28,6 +29,12 @@ export default class Welcome extends Command {
         default: 'Y',
       },
       {
+        name: 'isContent',
+        message: 'Do you want to install Directus as content management system?',
+        type: 'confirm',
+        default: 'Y',
+      },
+      {
         name: 'isRabbitMQ',
         message: 'Do you want to install RabbitMQ?',
         type: 'confirm',
@@ -36,11 +43,18 @@ export default class Welcome extends Command {
       directory = responses.name
       isAppsmith = responses.isAppsmith
       isRabbitMQ = responses.isRabbitMQ
+      isContent = responses.isContent
     }
     fs.mkdirSync(`./../${directory}`, {recursive: true})
     this.log('new directory created')
     try {
-      await execSync(`git clone -b 18-project-structure-for-chewy-stack https://gitlab.com/ephemerecreative/x-stack.git ./../${directory}`)
+      await execSync(`git clone -b 22-setup-subtree-for-the-project https://gitlab.com/ephemerecreative/x-stack.git ./../${directory}`)
+      if (isAppsmith) {
+        await execSync(`cd .. && cd ${directory} && git subtree add --prefix appsmith https://gitlab.com/swarnjitchahal/appsmith.git temp --squash`)
+      }
+      if (isContent) {
+        await execSync(`cd .. && cd ${directory} && git subtree add --prefix content https://gitlab.com/swarnjitchahal/content.git temp-content --squash`)
+      }
       // eslint-disable-next-line unicorn/catch-error-name
     } catch (err) {
       this.log('Error in new command', err.message)
