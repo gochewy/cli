d33a925cbe3a245c4789efdc2d2a59291cd02ef7
swarn <swarnchahal@Swarns-MacBook-Pro.local>
2021-04-30 13:44:28 -0400

    getting rid of functions related to creating files and running commands and use from Lib instead

--- src/commands/docker.ts
+++ src/commands/docker.ts
@@ -1,6 +1,6 @@
 import {Command} from '@oclif/command'
-const {execSync} = require('child_process')
-const config = require('./../../../kkk/project-config.json');
+import { execSync } from 'child_process'
+import Chewy from 'chewy-lib'
 
 export default class Docker extends Command {
   static description = 'Command to run docker containers'
@@ -10,20 +10,27 @@ export default class Docker extends Command {
   async run() {
     const {args} = this.parse(Docker)
     console.log(args.argument)
-    if (args.argument === 'start') {
-      if (config.dev.servicesEnabled.rabbitMQ)
-      execSync('docker-compose -f ./../kkk/docker-compose.yml -f ./../kkk/keycloak/docker-compose.yml -f ./../kkk/worker/docker-compose.yml up -d')
-      else {
-      execSync('docker-compose -f ./../kkk/docker-compose.yml -f ./../kkk/keycloak/docker-compose.yml -f ./../kkk/gql/docker-compose.yml up -d')
+    const dirName = require('./../../config.json').directory
+    const config = require(`./../../../${dirName}/project-config.json`);
+    if(dirName) {
+      if (args.argument === 'start') {
+        if (config.dev.worker)
+          execSync(`docker-compose -f ./../${dirName}/docker-compose.yml -f ./../${dirName}/keycloak/docker-compose.yml -f ./../${dirName}/worker/docker-compose.yml up -d`)
+        else {
+          execSync(`docker-compose -f ./../${dirName}/docker-compose.yml -f ./../${dirName}/keycloak/docker-compose.yml -f ./../${dirName}/gql/docker-compose.yml up -d`)
+        }
+      }
+      else if (args.argument === 'stop') {
+        if (config.dev.worker) {
+          execSync(`docker-compose -f ./../${dirName}/docker-compose.yml -f ./../${dirName}/keycloak/docker-compose.yml -f ./../${dirName}/worker/docker-compose.yml down`)
+        }
+        else {
+          execSync(`docker-compose -f ./../${dirName}/docker-compose.yml -f ./../${dirName}/keycloak/docker-compose.yml -f ./../${dirName}/gql/docker-compose.yml down`)
+        }
       }
     }
-    else if (args.argument === 'stop') {
-      if (config.dev.servicesEnabled.rabbitMQ) {
-        execSync('docker-compose -f ./../kkk/docker-compose.yml -f ./../kkk/keycloak/docker-compose.yml -f ./../kkk/worker/docker-compose.yml down')
-      }
-      else {
-        execSync('docker-compose -f ./../kkk/docker-compose.yml -f ./../kkk/keycloak/docker-compose.yml -f ./../kkk/gql/docker-compose.yml down')
-      }
+    else {
+      console.log("Directory not found")
     }
   }
 }
--- src/commands/welcome.ts
+++ src/commands/welcome.ts
@@ -2,70 +2,103 @@ import {Command} from '@oclif/command'
 import * as fs from 'fs'
 import * as inquirer from 'inquirer'
 import Chewy from 'chewy-lib'
+import * as chalk from 'chalk'
+import { cli } from 'cli-ux'
 const {execSync} = require('child_process')
 
 export default class Welcome extends Command {
   static description = 'Command to download required files'
 
-  static args = [{name: 'command'}]
+  static args = [{name: 'installOption'}]
 
   async run() {
-    // const {flags} = this.parse(Welcome)
+    const {args} = this.parse(Welcome)
     let directory
     let isAppsmith
     let isRabbitMQ
     let isContent
-    if (!directory) {
-      const responses: any = await inquirer.prompt([{
-        name: 'name',
-        message: 'name for your directory',
-        type: 'input',
-        default: 'chewy-stack',
-      },
-      {
-        name: 'isAppsmith',
-        message: 'Do you want to install appsmith?',
-        type: 'confirm',
-        default: 'Y',
-      },
-      {
-        name: 'isContent',
-        message: 'Do you want to install Directus as content management system?',
-        type: 'confirm',
-        default: 'Y',
-      },
-      {
-        name: 'isRabbitMQ',
-        message: 'Do you want to install RabbitMQ?',
-        type: 'confirm',
-        default: 'Y',
-      }])
-      directory = responses.name
-      isAppsmith = responses.isAppsmith
-      isRabbitMQ = responses.isRabbitMQ
-      isContent = responses.isContent
-    }
-    fs.mkdirSync(`./../${directory}`, {recursive: true})
-    this.log('new directory created')
-    try {
-      await execSync(`git clone -b 22-setup-subtree-for-the-project https://gitlab.com/ephemerecreative/x-stack.git ./../${directory}`)
-      if (isAppsmith) {
-        await execSync(`cd .. && cd ${directory} && git subtree add --prefix appsmith https://gitlab.com/swarnjitchahal/appsmith.git temp --squash`)
+    if (args.installOption === 'minimal') {
+      if (!directory) {
+        const responses: any = await inquirer.prompt([{
+          name: 'name',
+          message: 'what you want your project to be called',
+          type: 'input',
+          default: 'chewy-stack',
+        }])
+        directory = responses.name
       }
-      if (isContent) {
-        await execSync(`cd .. && cd ${directory} && git subtree add --prefix content https://gitlab.com/swarnjitchahal/content.git temp-content --squash`)
+      Chewy.File.createProjectDirectory(directory)
+      Chewy.Commands.installMinimalProject(directory)
+    }
+    else if (args.installOption === 'all') {
+      if (!directory) {
+        const responses: any = await inquirer.prompt([{
+          name: 'name',
+          message: 'what you want your project to be called',
+          type: 'input',
+          default: 'chewy-stack',
+        }])
+        directory = responses.name
       }
-      // eslint-disable-next-line unicorn/catch-error-name
-    } catch (err) {
-      this.log('Error in new command', err.message)
+      Chewy.File.createProjectDirectory(directory)
+      Chewy.Commands.installAllApps(directory)
     }
-    Chewy.File.envCreator(directory, 'client')
-    Chewy.File.envCreator(directory, 'server')
-    const answers = {
-      isAppsmith: isAppsmith,
-      isRabbitMQ: isRabbitMQ,
+    else if (args.installOption === 'custom') {
+      if (!directory) {
+        const responses: any = await inquirer.prompt([{
+          name: 'name',
+          message: 'what you want your project to be called',
+          type: 'input',
+          default: 'chewy-stack',
+        },
+          {
+            name: 'isAppsmith',
+            message: 'Do you want to install appsmith?',
+            type: 'confirm',
+            default: 'Y',
+          },
+          {
+            name: 'isServer',
+            message: 'Do you want to install backend server?',
+            type: 'confirm',
+            default: 'Y',
+          },
+          {
+            name: 'isContent',
+            message: 'Do you want to install Directus as content management system?',
+            type: 'confirm',
+            default: 'Y',
+          },
+          {
+            name: 'isRabbitMQ',
+            message: 'Do you want to install RabbitMQ?',
+            type: 'confirm',
+            default: 'Y',
+          }])
+        directory = responses.name
+        isAppsmith = responses.isAppsmith
+        isRabbitMQ = responses.isRabbitMQ
+        isContent = responses.isContent
+      }
+      const answers = {
+        isAppsmith: isAppsmith,
+        isRabbitMQ: isRabbitMQ,
+        isContent: isContent
+      }
+      Chewy.File.createProjectDirectory(directory)
+      Chewy.File.configFileGenerator(answers, directory)
+      Chewy.Commands.installCustomApps(directory)
+
+      Chewy.File.envCreator(directory, 'client')
+      Chewy.File.envCreator(directory, 'server')
+
+      Chewy.File.configFileGenerator(answers, directory)
+      Chewy.File.createConfig(directory)
+      this.log('directory is', directory, '----> and isAppsmith', isAppsmith)
     }
-    Chewy.File.fileGenerator(answers, directory)
-    this.log('directory is', directory, '----> and isAppsmith', isAppsmith)
+    else {
+      this.log(chalk.redBright('No option or invalid option provided'))
+      chalk.greenBright(Chewy.Utils.printInstallOptions())
+    }
   }
 }
