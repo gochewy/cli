1fab298297d14fc703b7940f37fc9e226aeb0c14
Raphaël Titsworth-Morin <raphael@raphaeltm.com>
2023-03-21 03:13:43 +0000

    switched back to prod

--- .husky/pre-commit.js
+++ .husky/pre-commit.js
@@ -2,7 +2,7 @@ const { readFileSync } = require("fs");
 const { resolve } = require("path");
 
 const content = readFileSync(resolve('package.json'))
-const hasLocalLib = content.includes('"@gochewy/lib": "../lib/"') || content.includes('"@gochewy/lib": "../lib"') || content.includes('"@gochewy/lib": "file:../lib"')
+const hasLocalLib = content.includes('"../lib/"') || content.includes('"file:/workspace/')
 
 if(hasLocalLib){
     throw new Error('🛑🛑🛑 DO NOT COMMIT WITH LOCAL LIB 🛑🛑🛑\nTry running `yarn lprod`')
